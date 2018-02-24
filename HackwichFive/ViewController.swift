cb5c47a6c4e0381499a13ef6f2deecb3db0d350e
Melody <melodyio@hawaii.edu>
2018-02-24 00:29:32 -1000

    Declared array, more lines of code

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    //Declare your array here
+
+    var myFriendsArray = ["Emily","Courtney","George"]
+    
+    
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
+        self.tableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+    
+        return 1;
+        
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return myFriendsArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+    
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseldentifier")!
+            let text = myFriendsArray[indexPath.row]
+            cell.textLabel?.text = text
+            return cell
+            }
 }
-
